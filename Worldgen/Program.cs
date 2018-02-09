e7de059f6ed3cf9f591aa3760565227b4350637d
Rob <Rob@LAPTOP-JAU16JQ6>
2018-02-09 16:59:28 +0100

    start

---
+++ Worldgen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worldgen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testje
+            int A;
+            int B;
+        }
+    }
+}
